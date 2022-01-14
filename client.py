4ebab744fbf37bc3767feb6d4eeee93ac67d5fd5
Simran Kumari <42550408+cyberspace-simran@users.noreply.github.com>
2022-01-14 11:17:00 +0530

    Add files via upload

--- client.py
+++ client.py
@@ -1,23 +1,48 @@
 import socket
 import json
 import subprocess
+import os
+from uuid import uuid4
 
-
-
-c=socket.socket()
+c = socket.socket()
 c.connect(('localhost', 9999))
 
-
 while True:
-    name = input("Enter your name")
-    c.send(bytes(name, 'utf-8'))
+    # name = input("Enter your name")
+    # c.send(bytes(name, 'utf-8'))
 
     cmd = input("Enter the os command")
+    data = {'command': cmd, 'id': str(uuid4())}
+
     c.send(bytes(cmd, 'utf-8'))
+    strData = json.dumps(data)
+    c.send(bytes(strData, 'utf-8'))
 
-    # data = {'command': cmd, 'id': '1'}
-    # strData = json.dumps(data)
-    # c.send((bytes(strData,'utf-8'))
-    #tried it with json but was unsuccessful
+    cmd = c.recv(2048).decode("utf-8")
+    errcode = -1
 
-client.close()
+    try:
+        # data = json.loads(cmd)
+        strData = json.dumps(data)
+
+    except:
+        errcode = 1
+
+    if errcode == -1:
+        print("command is:", data['command'])
+
+        result = ""
+        stdout = ""
+        try:
+            result = os.system(data["command"])
+            stdout = subprocess.run(data["command"], stdout=subprocess.PIPE).stdout.decode('utf-8')
+            print("standard output:", stdout)
+        except:
+            errcode = 3
+
+        id = data["id"]
+        to_send = {"result": result, "stdout": stdout, "errcode": errcode, "id": id}
+        str_to_send = json.dumps(to_send)
+        c.send(str_to_send.encode("utf-8"))
+
+c.close()
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import json
 import subprocess
+from uuid import uuid4
+
 
 s = socket.socket()
 print("Socket created")
@@ -13,15 +15,20 @@ c, addr = s.accept()
 print("connected")
 while True:
 
-    name = c.recv(1024).decode()
+    # name = c.recv(1024).decode()
+
     cmd = c.recv(1024).decode()
     subprocess.Popen(cmd, shell=True)
+    strData = c.recv(1024).decode()
 
-    # data = {'command': cmd, 'id': '1'}
-    # strData = json.dumps(data)
-    # strData = c.recv(1024).decode()
+    print("connected with",addr)
+    print(strData)
 
-    print("connected with",addr,"name", name,"command", cmd)
+    # print("NEW CONNECTION:", addr)
+    c.send(strData.encode("utf-8"))
+
+    clientResponse = c.recv(2048).decode("utf-8")
+    print("Client response:", clientResponse)
 
 
     c.send(bytes("We are connected", "utf -8 "))
